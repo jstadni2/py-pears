48322dde918ae34ef23f145292bdcb04d5d5d3c0
Jamie Stadnik <jstadni2@illinois.edu>
2022-10-04 15:56:25 -0500

    Implement date triggers in schedule.py

--- py_pears/schedule.py
+++ py_pears/schedule.py
@@ -1,4 +1,5 @@
 import os
+from datetime import date
 import py_pears.utils as utils
 import py_pears.reports.sites_report as sites_report
 import py_pears.reports.staff_report as staff_report
@@ -28,46 +29,77 @@ update_notifications = TEST_INPUTS_DIR + 'Update Notifications.xlsx'
 
 creds = utils.load_credentials()
 
+# Refactor schedule using OOP?
+
+
+def compare_date(left_date=date.today(),
+                 year=date.today().year,
+                 month=date.today().month,
+                 day=date.today().day):
+    return left_date == date(year, month, day)
+
+
+def compare_date_quarterly(days):
+    quarter_start_months = [1, 4, 7, 10]
+    for month in quarter_start_months:
+        for day in days:
+            if compare_date(month=month, day=day):
+                return True
+    return False
+
+
+# Monthly Reports
+
 # Run Sites Report with default inputs
-sites_report.main(creds=creds, export_dir=EXPORT_DIR, output_dir=OUT_DIR)
+if compare_date(day=2):
+    sites_report.main(creds=creds, export_dir=EXPORT_DIR, output_dir=OUT_DIR)
 
 # Run Staff Report with default inputs
-staff_report.main(creds=creds, export_dir=EXPORT_DIR, output_dir=OUT_DIR, staff_list=staff_list)
+if compare_date(day=11):
+    staff_report.main(creds=creds, export_dir=EXPORT_DIR, output_dir=OUT_DIR, staff_list=staff_list)
 
 # Run Monthly Data Cleaning with default inputs
-monthly_data_cleaning.main(creds=creds,
-                           export_dir=EXPORT_DIR,
-                           output_dir=OUT_DIR,
-                           staff_list=staff_list,
-                           names_list=names_list,
-                           unit_counties=unit_counties,
-                           update_notifications=update_notifications)
-
-# Run Quarterly Program Evaluation with default inputs
-quarterly_program_evaluation.main(creds=creds, export_dir=EXPORT_DIR, output_dir=OUT_DIR)
-
-# Run Partnerships Entry with default inputs
-partnerships_entry.main(creds=creds,
-                        export_dir=EXPORT_DIR,
-                        output_dir=OUT_DIR,
-                        staff_list=staff_list,
-                        unit_counties=unit_counties,
-                        prev_year_part_export=TEST_INPUTS_PEARS_PREV_YEAR_DIR + 'Partnership_Export.xlsx')
-
-# Run Coalition Survey Cleaning with default inputs
-coalition_survey_cleaning.main(creds=creds,
+if compare_date(day=12):
+    monthly_data_cleaning.main(creds=creds,
                                export_dir=EXPORT_DIR,
                                output_dir=OUT_DIR,
-                               coalition_surveys_dir=TEST_INPUTS_PEARS_COA_SURVEYS_DIR,
                                staff_list=staff_list,
+                               names_list=names_list,
                                unit_counties=unit_counties,
                                update_notifications=update_notifications)
+
+# Run Monthly Partnerships Entry with default inputs
+if compare_date(day=20):
+    partnerships_entry.main(creds=creds,
+                            export_dir=EXPORT_DIR,
+                            output_dir=OUT_DIR,
+                            staff_list=staff_list,
+                            unit_counties=unit_counties,
+                            prev_year_part_export=TEST_INPUTS_PEARS_PREV_YEAR_DIR + 'Partnership_Export.xlsx')
+
+# Quarterly Reports
+
+# Run Coalition Survey Cleaning with default inputs
+if compare_date_quarterly(days=[12, 23]):
+    coalition_survey_cleaning.main(creds=creds,
+                                   export_dir=EXPORT_DIR,
+                                   output_dir=OUT_DIR,
+                                   coalition_surveys_dir=TEST_INPUTS_PEARS_COA_SURVEYS_DIR,
+                                   staff_list=staff_list,
+                                   unit_counties=unit_counties,
+                                   update_notifications=update_notifications)
+
+# Run Quarterly Program Evaluation with default inputs
+if compare_date_quarterly(days=[13]):
+    quarterly_program_evaluation.main(creds=creds, export_dir=EXPORT_DIR, output_dir=OUT_DIR)
+
 
 # Annual Reports
 
 # Run Partnerships Intervention Type Cleaning with default inputs
-partnerships_intervention_type.main(creds=creds,
-                                    export_dir=EXPORT_DIR,
-                                    output_dir=OUT_DIR,
-                                    staff_list=staff_list,
-                                    )
+if compare_date(month=10, day=4):
+    partnerships_intervention_type.main(creds=creds,
+                                        export_dir=EXPORT_DIR,
+                                        output_dir=OUT_DIR,
+                                        staff_list=staff_list,
+                                        )
